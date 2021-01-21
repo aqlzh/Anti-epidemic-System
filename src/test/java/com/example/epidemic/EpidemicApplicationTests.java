a98ce95d084a3429456619ffb5360f5055e997f9
子非林 <xyl342300@163.com>
2021-01-21 21:33:10 +0800

    zifl first commit

---
+++ src/test/java/com/example/epidemic/EpidemicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epidemic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
