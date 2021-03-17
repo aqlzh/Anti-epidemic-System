9859961830730e8c072db1a10d9219883c929563
子非林 <xyl342300@163.com>
2021-03-17 17:21:31 +0800

    疫情信息实体类完善

--- src/main/java/com/example/epi/model/EpidemicInfo.java
+++ src/main/java/com/example/epi/model/EpidemicInfo.java
@@ -6,7 +6,6 @@ import lombok.Data;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
@@ -14,19 +13,34 @@ import java.util.Date;
 @Data
 @Table(name = "epidemic_info")
 public class EpidemicInfo implements Serializable {
-    @Id
+    /*@Id
     @Column(name = "id")
-    private int id;
+    private int id;*/
     @Basic
     @Column(name = "name")
     private String name;
     @Basic
+    @Column(name = "age")
+    private int age;
+    @Basic
+    @Column(name = "sex")
+    private String sex;
+    @Basic
+    @Column(name = "identy")
+    private String identy;
+    @Basic
+    @Column(name= "temp")
+    private String temp;
+    @Basic
+    @Column(name="inNative")
+    private String inNative;
+    @Basic
+    @Column(name = "adress")
+    private String adress;
+    @Basic
     @Column(name = "regtime")
     @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss",locale = "zh",timezone = "GMT+8")
     private Date regtime;
-/*    @Basic
-    @Column(name = "status")
-    private int status;*/
     @Basic
     @Column(name = "id_type")
     private String id_type;
@@ -36,22 +50,8 @@ public class EpidemicInfo implements Serializable {
     @Basic
     @Column(name = "phone")
     private long phone;
-    @Basic
-    @Column(name = "age")
-    private int age;
-    @Basic
-    @Column(name = "sex")
-    private int sex;
-    @Basic
-    @Column(name="inNative")
-    private String inNative;
-    @Basic
-    @Column(name = "adress")
-    private String adress;
-
-    @Basic
-    @Column(name = "temp")
-    private double temp;
-
+    /*    @Basic
+        @Column(name = "status")
+        private int status;*/
 
 }
