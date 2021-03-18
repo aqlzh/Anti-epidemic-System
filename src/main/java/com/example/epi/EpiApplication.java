12cdee585d7fad6ee8f6b4677a8c31f3e0311598
子非林 <xyl342300@163.com>
2021-03-18 16:47:23 +0800

    添加启动类注解

--- src/main/java/com/example/epi/EpiApplication.java
+++ src/main/java/com/example/epi/EpiApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @MapperScan("com.example.epi.mapper")
 @ServletComponentScan("com.example.epi.servlet")
 public class EpiApplication {
